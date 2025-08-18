27d139e66bc64aed1d189b0e5463db13fb776dcb
George <giorgitsereteli541@gmail.com>
2025-08-18 17:53:40 +0400

    created mobile navigation bar

--- marusa_line/src/app/layout/header/header.component.ts
+++ marusa_line/src/app/layout/header/header.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AppRoutes } from '../../shared/components/cards/AppRoutes/AppRoutes';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   AppRoutes = AppRoutes;
   scrollToBottom(): void {
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: 'smooth' 
-  });
-}
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth' 
+    });
+    this.hideSidenav();
+  }
 
+
+  sidenavVisible:boolean = false;
+  leftToright:boolean = false;
+  openSidenav(){
+    this.sidenavVisible =  true;
+    this.leftToright = false;
+  }
+  hideSidenav(){
+    this.leftToright = true;
+    setTimeout(() => {
+      this.sidenavVisible =  false;
+    }, 500);
+  }
 }
